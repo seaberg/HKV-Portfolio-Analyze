b58d56908cbed95fde1cb853876a133f1b1f3134
Jonas Sjöberg <jonas.sj@icloud.com>
2017-11-05 09:38:29 +0100

    HKVTransaction

--- src/main/java/com/example/demo/HKVTransaction.java
+++ src/main/java/com/example/demo/HKVTransaction.java
@@ -1,13 +1,43 @@
 package com.example.demo;
 
+import java.math.BigDecimal;
+import java.sql.Date;
+
 public class HKVTransaction {
 	private String StockName;
-	
-	public void setStockName(String stockName) {
-		StockName = stockName;
-	}
+	private Date TransactionDate;
+	private BigDecimal Price;
+	private BigDecimal TransactionFee;
+	private int Amount;
 	
 	public String getStockName() {
 		return StockName;
+	}
+	public void setStockName(String stockName) {
+		StockName = stockName;
+	}
+	public Date getTransactionDate() {
+		return TransactionDate;
+	}
+	public void setTransactionDate(Date transactionDate) {
+		TransactionDate = transactionDate;
+	}
+	public BigDecimal getPrice() {
+		return Price;
+	}
+	public void setPrice(BigDecimal price) {
+		Price = price;
+	}
+	public BigDecimal getTransactionFee() {
+		return TransactionFee;
+	}
+	public void setTransactionFee(BigDecimal transactionFee) {
+		TransactionFee = transactionFee;
+	}
+	public int getAmount() {
+		return Amount;
+	}
+	public void setAmount(int amount) {
+		Amount = amount;
 	}
 }
