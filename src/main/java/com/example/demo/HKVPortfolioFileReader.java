fdaa8572f0e6448cad0055a248d0a4dd1b861e0b
Jonas Sjöberg <jonas.sj@icloud.com>
2017-11-05 12:37:02 +0100

    HKVPortfolioReader

--- src/main/java/com/example/demo/HKVPortfolioFileReader.java
+++ src/main/java/com/example/demo/HKVPortfolioFileReader.java
@@ -1,15 +1,36 @@
 package com.example.demo;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 public class HKVPortfolioFileReader {
 	
 	public void ReadFile() {
 		System.out.println("Hej hej!");
 		
 		//Open file
+		String contents = null;
 		
-		//Read lines
+		try {
+			contents = new String(Files.readAllBytes(Paths.get("src/main/resources/portfolios/wt1.hkp")), StandardCharsets.ISO_8859_1);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		//If 
+		//System.out.println(contents);
+		
+		//Split lines
+		String[] lines = contents.split("\n");
+		
+		for(int i = 0; i < lines.length; i++)
+		{
+			System.out.println(lines[i]);
+		}
+		
+		//Check line transaction type
 	}
 	
 	public int Number() {
