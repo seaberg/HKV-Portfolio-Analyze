5e16c09f55e36f7ebf5245392e6e73a5fe630699
Jonas Sjöberg <jonas.sj@icloud.com>
2017-11-05 08:53:17 +0100

    HKVPortfolioReader

---
+++ src/main/java/com/example/demo/HKVPortfolioFileReader.java
@@ -0,0 +1,12 @@
+package com.example.demo;
+
+public class HKVPortfolioFileReader {
+	
+	public void ReadFile() {
+		
+	}
+	
+	public int Number() {
+		return 1;
+	}
+}
--- src/test/java/com/example/demo/HkvPortfolioAnalyzeApplicationTests.java
+++ src/test/java/com/example/demo/HkvPortfolioAnalyzeApplicationTests.java
@@ -5,12 +5,23 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import junit.framework.Assert;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class HkvPortfolioAnalyzeApplicationTests {
 
+//	@Test
+//	public void contextLoads() {
+//	}
+	
 	@Test
-	public void contextLoads() {
+	public void NumberTest() {
+		HKVPortfolioFileReader preader = new HKVPortfolioFileReader();
+		
+		int result = preader.Number();
+		
+		Assert.assertEquals(1, result);
 	}
 
 }
